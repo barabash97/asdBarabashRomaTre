603f91b91d4cac2b4ca32c57417e483ffb2dcaf6
Volodymyr Barabash <barabash97@gmail.com>
2017-10-19 15:20:43 +0200

    19 10 17

--- c_projects/programmingc/recor_selection_sort/main.c
+++ c_projects/programmingc/recor_selection_sort/main.c
@@ -4,6 +4,68 @@
 
 int random_array(int array[], int dim, int max_num);
 
+
+/*
+ *  Funzione Merge per la fusione di due
+ *  componenti ordinate dell'array.
+ */
+
+void Merge(int A[], int p, int q, int r, int dim) {
+  int i, j, k, B[dim];
+  i = p;
+  j = q+1;
+  k = 0;
+  while (i<=q && j<=r) {
+    if (A[i]<A[j]) {
+      B[k] = A[i];
+      i++;
+    } else {
+      B[k] = A[j];
+      j++;
+    }
+    k++;
+  }
+  while (i <= q) {
+    B[k] = A[i];
+    i++;
+    k++;
+  }
+  while (j <= r) {
+    B[k] = A[j];
+    j++;
+    k++;
+  }
+  for (k=p; k<=r; k++)
+    A[k] = B[k-p];
+  return;
+}
+
+
+/*
+ *  Funzione ricorsiva MergeSort.
+ */
+
+void MergeSort(int A[], int p, int r, int dim) {
+  int q;
+  if (p < r) {
+    q = (p+r)/2;
+    MergeSort(A, p, q, dim);
+    MergeSort(A, q+1, r, dim);
+    Merge(A, p, q, r, dim);
+  }
+  return;
+}
+
+/**
+ * @brief Stampa array
+ * @param array
+ * @param dim
+ */
+void stampaArray(int array[], int dim){
+    for(int i = 0; i < dim; i++){
+        printf("%d\n", array[i]);
+    }
+}
 int main(int argc, char **argv)
 {
 	int i, dim, max_num_random; //Inizializzazione variabili
@@ -19,10 +81,14 @@ int main(int argc, char **argv)
     
     random_array(array, dim, max_num_random);
     
-    printf("\n\n#####################\n\n");
-    for(i = 0; i < dim; i++){
-        printf("%d\n", array[i]);
-    }
+    printf("\n\n######### NON ORDINATO  ############\n\n");
+    stampaArray(array, dim);
+    printf("\n\n######### FINE NON ORDINATO  ############\n\n");
+    MergeSort(array, 0,(dim/2) + 1, dim, dim);
+     printf("\n\n#########  ORDINATO  ############\n\n");
+    stampaArray(array, dim);
+    printf("\n\n######### FINE ORDINATO  ############\n\n");
+    stampaArray(array, dim);
     
     
 	return 0;
