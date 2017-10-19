50dd2b6d0dd53837bd027301105d4f979f0e3c4a
Volodymyr Barabash <barabash97@gmail.com>
2017-10-19 15:35:06 +0200

    commit

---
+++ c_projects/programmingc/testExit/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void testA(){
+    printf("testA\n");
+}
+
+void testB(){
+    printf("testB\n");
+}
+int main(int argc, char **argv)
+{
+	testA();
+    testB();
+	return 0;
+}
