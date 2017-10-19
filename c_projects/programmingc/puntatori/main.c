cbda516c72e8f59c5f46f20ed7932d6f849ff355
Volodymyr Barabash <barabash97@gmail.com>
2017-10-19 16:14:43 +0200

    random array

--- c_projects/programmingc/puntatori/main.c
+++ c_projects/programmingc/puntatori/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+ * Const 
+ * */
+#define DIM 10
+#define MAX_VALUE 100
+
+void puntatori();
+int random_array(int array[], int dim, int max_num);
+void stampaArrayInteri(int *array, int dim);
 
 int main(int argc, char **argv)
 {
-	int intero = 100;
+	int array[DIM];
+    random_array(array, DIM, MAX_VALUE);
+    stampaArrayInteri(array, DIM);
+	return 0;
+}
+
+/**
+ * @brief Puntatori
+ */
+void puntatori(){
+    int intero = 100;
     
     int* a = &(intero); //Puntatore
     int** b = &a; //Puntatore a puntatore
@@ -14,10 +36,38 @@ int main(int argc, char **argv)
     
     **b = 300;
     printf("%d\n", intero); //300
-	return 0;
 }
-
 /**
+ * Puntatori:
     "*" => il contenuto
     "&" => indirizzo    
 */
+
+/**
+ * @brief Array randomico
+ * @param array
+ * @param dim
+ * @param max_num
+ * @return 
+ */
+int random_array(int array[], int dim, int max_num){
+    
+    int i;
+    
+    time_t t;
+    srand((unsigned) time(&t));
+    
+    for(i = 0; i < dim; i++){
+        array[i] = rand() % max_num;
+    }
+}
+
+void stampaArrayInteri(int *array, int dim){
+    printf("\n\n############## STAMPA ARRAY ##################\n");
+    
+    for(int i = 0; i < dim; i++){
+        printf("%d\n", array[i]);
+    }
+    
+    printf("\n\n############## FINE STAMPA ARRAY ##################\n");
+}
